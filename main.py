08224a7e28d71e623ab9191636e37595c0510377
彭涛 <pengtaopt01@163.com>
2018-05-15 21:20:13 +0800

    auto_friends_accpet

--- main.py
+++ main.py
@@ -62,58 +62,59 @@ def group_text(msg):
 # # 自动通过好友验证
 @bot.register(msg_types = FRIENDS)
 def auto_accept_friends(msg):
-    print(msg)
-    # 通过好友验证
-    new_friend = msg.card.accept()
-    # 获取生成的备注
-    ramerkName = ort.generateRemarkName(bot)
-    # 修改备注
-    bot.core.set_alias(userName=msg.raw['RecommendInfo']['UserName'], alias=ramerkName)
-    # 获取邀请人
-    soup = BeautifulSoup(msg.raw.get('Content'), 'lxml')
-    msg_soup = soup.find('msg')
-    # 邀请人昵称
-    sourcname = msg_soup.get('sourcenickname')
-    # 被邀请人puid
-    user_wxid = new_friend.puid
-    if sourcname == '':
-        sourc = 0
-    else:
-        # 获取邀请人puid
-        lnivt_user = bot.friends().search(sourcname)[0]
-        sourc = lnivt_user.puid
+    if config.get('SYS', 'auto_accept_friends') == 'yes':
+        print(msg)
+        # 通过好友验证
+        new_friend = msg.card.accept()
+        # 获取生成的备注
+        ramerkName = ort.generateRemarkName(bot)
+        # 修改备注
+        bot.core.set_alias(userName=msg.raw['RecommendInfo']['UserName'], alias=ramerkName)
+        # 获取邀请人
+        soup = BeautifulSoup(msg.raw.get('Content'), 'lxml')
+        msg_soup = soup.find('msg')
+        # 邀请人昵称
+        sourcname = msg_soup.get('sourcenickname')
+        # 被邀请人puid
+        user_wxid = new_friend.puid
+        if sourcname == '':
+            sourc = 0
+        else:
+            # 获取邀请人puid
+            lnivt_user = bot.friends().search(sourcname)[0]
+            sourc = lnivt_user.puid
 
-    ort.create_user_info(msg, bot, msg.raw, lnivt_code=sourc, tool=True, wxid=user_wxid, sourcname=sourcname)
-    text = '''
-Hi~我是24h在线的淘小券机器人，用淘小券，免费领取任意淘宝、京东商品优惠券，好用的话记得分享给好友哦
+        ort.create_user_info(msg, bot, msg.raw, lnivt_code=sourc, tool=True, wxid=user_wxid, sourcname=sourcname)
+        text = '''
+    Hi~我是24h在线的淘小券机器人，用淘小券，免费领取任意淘宝、京东商品优惠券，好用的话记得分享给好友哦
 
-回复【帮助】可查询指信息
-回复【提现】申请账户余额提现
-回复【推广】可申请机器人代理
-回复【个人信息】可看个当前账户信息
+    回复【帮助】可查询指信息
+    回复【提现】申请账户余额提现
+    回复【推广】可申请机器人代理
+    回复【个人信息】可看个当前账户信息
 
-回复【买+商品名称】
-回复【找+商品名称】
-回复【搜+商品名称】查看商品优惠券合集
+    回复【买+商品名称】
+    回复【找+商品名称】
+    回复【搜+商品名称】查看商品优惠券合集
 
-分享【京东商品链接】或者【淘口令】
-精准查询商品优惠券和返利信息！
-分享【VIP视频链接】免费查看高清VIP视频！
+    分享【京东商品链接】或者【淘口令】
+    精准查询商品优惠券和返利信息！
+    分享【VIP视频链接】免费查看高清VIP视频！
 
-优惠券使用教程：
-'''+config.get('URL', 'course')+'''
-跑堂优惠券常见问题：
-'''+config.get('URL', 'faq')+'''
-免费看电影方法：
-'''+config.get('URL', 'movie')+'''
-京东优惠券商城：
-'''+config.get('URL', 'jdshop')+'''
-淘宝优惠券商城：
-'''+config.get('URL', 'tbshop')+'''
-邀请好友得返利说明：
-'''+config.get('URL', 'lnvit')+'''
-            '''
-    new_friend.send(text)
+    优惠券使用教程：
+    '''+config.get('URL', 'course')+'''
+    跑堂优惠券常见问题：
+    '''+config.get('URL', 'faq')+'''
+    免费看电影方法：
+    '''+config.get('URL', 'movie')+'''
+    京东优惠券商城：
+    '''+config.get('URL', 'jdshop')+'''
+    淘宝优惠券商城：
+    '''+config.get('URL', 'tbshop')+'''
+    邀请好友得返利说明：
+    '''+config.get('URL', 'lnvit')+'''
+                '''
+        new_friend.send(text)
 
 
 if __name__ =='__main__':
